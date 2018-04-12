f033b039153c8e12925342361ea12687ba0687d2
AlexKnauth <alexander@knauth.org>
2018-04-12 13:39:39 -0400

    fix test

--- test/test-where.rkt
+++ test/test-where.rkt
@@ -2,7 +2,7 @@
 
 (define-signature A = (sig (type X) (val x : X) (val f : (-> X Int))))
 (define-signature B = (sig (type X) (type Y) (val y : Y) (val f : (-> Y X))))
-(define-signature C = (sig (type X) (val f-y : X)))
+(define-signature C = (sig (type X) (val ffy : Int)))
 
 (define-module A* =
   (seal (mod (type X = Int)
@@ -12,21 +12,21 @@
         A))
 
 (define-module B*/A =
-  (λ ([A* : A])
+  (λ ([A : A])
     (seal
-     (mod (type X = A*.X)
+     (mod (type X = A.X)
           (type Y = (-> X))
-          (val y : Y = (λ () A*.x))
+          (val y : Y = (λ () A.x))
           (val f : (-> Y X) = (λ (y) (y))))
      :>
-     (where B X = A*.X))))
+     (where B X = A.X))))
 
 (define-module C*/AB =
-  (λ ([A* : A])
-    (λ ([B* : (where B Y = A*.X)])
+  (λ ([A : A])
+    (λ ([B : (where B X = A.X)])
       (seal
-       (mod (type X = A*.X)
-            (val ffy : X = (A*.f (B*.f B*.y))))
+       (mod (type X = A.X)
+            (val ffy : Int = (A.f (B.f B.y))))
        :>
-       (where C X = A*.X)))))
+       (where C X = A.X)))))
 

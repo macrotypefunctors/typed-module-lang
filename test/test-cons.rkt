66f7064a0d59a6f25d30bc9332ed79725be54337
Milo Turner <iitalics@gmail.com>
2018-04-12 18:08:15 -0400

    List example from slides

--- test/test-cons.rkt
+++ test/test-cons.rkt
@@ -1,41 +1,45 @@
 #lang typed-module-lang
 
-(define-signature PAIR =
+(define-signature LIST =
   (sig (type T)
-       (val cons : (-> Int Int T))
-       (val car : (-> T Int))
-       (val cdr : (-> T Int))))
+       (val empty : T)
+       (val cons : (-> Int T T))
+       (val empty? : (-> T Bool))
+       (val first : (-> T Int))
+       (val rest : (-> T T))))
 
 (define-module TestPair =
-  (λ ([P : PAIR])
-    (mod (val p1 : P.T = (P.cons 7 11))
-         (val p2 : P.T = (P.cons 15 4))
-         (check (P.car p1) = 7)
-         (check (P.cdr p1) = 11)
-         (check (P.car p2) = 15)
-         (check (P.cdr p2) = 4))))
+  (λ ([L : LIST])
+    (mod (val list1 : L.T = (L.cons 2 L.empty))
+         (val list2 : L.T = (L.cons 1 list1))
+         (check (L.empty? L.empty) = #t)
+         (check (L.empty? list1) = #f)
+         (check (L.first list1) = 2)
+         (check (L.rest list2) = list1))))
 
 (define-module PrimeExpt =
-  (mod
-   (type T = Int)
-   (val car : (-> T Int) =
-     (λ (x)
-       (if (= (rem x 2) 0)
-           (+ 1 (car (quo x 2)))
-           0)))
+  (seal
+   (mod (type T = Int)
+        (val empty : T = 0)
+        (val cons : (-> Int T T) =
+          (λ (x y)
+            (if (= x 0)
+                (if (= y 0) 1
+                    (* 2 (cons x (- y 1))))
+                (* 3 (cons (- x 1) y)))))
 
-   (val cdr : (-> T Int) =
-     (λ (x)
-       (if (= (rem x 3) 0)
-           (+ 1 (cdr (quo x 3)))
-           0)))
+        (val empty? : (-> T Bool) =
+          (λ (n) (= n 0)))
 
-   (val cons : (-> Int Int T) =
-     (λ (x y)
-       (if (= x 0)
-           (if (= y 0)
-               1
-               (* 3 (cons 0 (- y 1))))
-           (* 2 (cons (- x 1) y)))))))
+        (val first : (-> T Int) =
+          (λ (n) (if (= (rem n 3) 0)
+                     (+ 1 (first (quo n 3)))
+                     0)))
+
+        (val rest : (-> T Int) =
+          (λ (n) (if (= (rem n 2) 0)
+                     (+ 1 (rest (quo n 2)))
+                     0))))
+   :> LIST))
 
 (define-module TestPrimeExpt = (TestPair PrimeExpt))

b5d83c8d500340faaa93ef854566da08f1de3dbc
AlexKnauth <alexander@knauth.org>
2018-05-10 17:38:15 -0400

    continue on link-instance stuff

--- lib/typed-module-lang/link/link-instance.rkt
+++ lib/typed-module-lang/link/link-instance.rkt
@@ -7,12 +7,35 @@
 ;; This is a "Link-time" module
 
 ;; An InstanceContext is a
+;;   (Boxof InstanceMapping)
+(define BASE-INSTANCE-CONTEXT (box-immutable '()))
+
+;; An InstanceMapping is a
 ;;   [Listof InstanceEntry]
 
 ;; An InstanceEntry is one of:
 ;;  - [List Constraint InstanceDict]
 ;; TODO: include "qualified" constraints from instance chaining,
 ;;       which have "dictionary functions" from instance chaining
+
+;; A Constraint is a (constraint Class Tag)
+
+;; A Class is a:
+;;  - (class Label InstanceContext)
+(struct class [label instance-context])
+
+;; A Tag is one of:
+;;  - 'Int
+;;  - 'Bool
+;;  - 'Arrow
+;;  - (data Label InstanceContext)
+(struct data [label instance-context])
+
+;; Class Class -> Bool
+(define class=? equal?)
+
+;; Tag Tag -> Bool
+(define tag=? equal?)
 
 ;; InstanceEntry -> Constraint
 ;; this will get slightly more complicated with "qualified" constraints,
@@ -21,17 +44,29 @@
 
 ;; -------------------------------------
 
-;; for every type there is an InstanceContext
-;; type-instance-context : Type -> InstanceContext
-(define (type-instance-context type)
-  ('....))
+;; for every type there is a tag, and
+;; for every tag there is an InstanceMapping
+;; tag-instance-mapping : Tag -> InstanceMapping
+(define (tag-instance-mapping tag)
+  (match tag
+    [(or 'Int 'Bool 'Arrow) BASE-INSTANCE-CONTEXT]
+    [(data label ctx) (unbox ctx)]))
 
-;; for every class there is an InstanceContext
-;; class-instance-context : Class -> InstanceContext
-(define (class-instance-context class)
-  ('....))
+;; for every class there is an InstanceMapping
+;; class-instance-mapping : Class -> InstanceMapping
+(define (class-instance-mapping c)
+  (match c
+    [(class label ctx) (unbox ctx)]))
 
 ;; -------------------------------------
+
+#|
+; (class (Cond X) [test : (-> X Bool)])
+; (instance (forall (X) (Cond X) => (Cond (-> X)))
+;   ...)
+
+; at LT:  (lookup-instance (Cond (-> Int)))
+|#
 
 ;; looking up an instance for a constraint looks in two places:
 ;;  - the type's instance context
@@ -42,9 +77,9 @@
 ;; That means the constraint should be guaranteed to be valid.
 ;; lookup-instance : Constraint -> InstanceDict
 (define (lookup-instance con)
-  (match-define (constraint class type) con)
-  (define c-ctx (class-instance-context class))
-  (define t-ctx (type-instance-context type))
+  (match-define (constraint class tag) con)
+  (define c-ctx (class-instance-mapping class))
+  (define t-ctx (tag-instance-mapping tag))
   (define ent
     (or
      (lookup-instance-entry/context con c-ctx)
@@ -55,7 +90,7 @@
       (error 'lookup-instance
              "TODO: solve \"subgoals\" for instance chaining")))
 
-;; lookup-instance-entry/context : Constraint InstanceContext -> [Maybe InstanceEntry]
+;; lookup-instance-entry/context : Constraint InstanceMapping -> [Maybe InstanceEntry]
 (define (lookup-instance-entry/context con ctx)
   (match-define (constraint class type) con)
   (for/or ([ent (in-list ctx)])
@@ -65,11 +100,9 @@
 
 ;; constraint-matches? : Constraint Constraint -> Bool
 (define (constraint-matches? con1 con2)
-  (match-define (constraint class1 type1) con1)
-  (match-define (constraint class2 type2) con2)
-  ;; TODO: environment? at link time? or should it be "resolved" by now?
-  ;; TODO: should subtyping play a role in this? which direction?
+  (match-define (constraint class1 tag1) con1)
+  (match-define (constraint class2 tag2) con2)
   (and (class=? class1 class2)
-       (type=? type1 type2)))
+       (tag=? tag1 tag2)))
 
 
